3886403dde58a11ee6883c8356038601bcf180fd
kudinov_89 <vlad.kudinov.89@gmail.com>
2020-06-05 21:50:17 +0300

    Cleanup code.

--- app/Services/User/UserService.php
+++ app/Services/User/UserService.php
@@ -16,27 +16,17 @@ class UserService
         $data = Http::get('https://randomuser.me/api/?results=100&gender=male&inc=gender,name,email')
             ->json();
 
-        $collection = collect($data['results']);   //turn data into collection
-        $chunks = $collection->chunk(10); //chunk into smaller pieces
-        $chunks->toArray(); //convert chunk to array
+        $collection = collect($data['results']);
+        $chunks = $collection->chunk(10);
+        $chunks->toArray();
 
-//loop through chunks:
         foreach($chunks as $chunk)
         {
-//            dd($chunk->toArray());
-//            dd($chunk);
-
             foreach ($chunk as $item) {
-//                dd($item['name']);
-//                $name = $this->presenterName($item['name']);
                 $item['name'] = $this->presenterName($item['name']);
                 User::insert($item); //insert chunked data
             }
-
-//            User::insert($chunk->toArray()); //insert chunked data
         }
-
-//        dd(User::all());
     }
     private function presenterName(array $name) : string
     {
